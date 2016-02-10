b9f5be728cb75bd1740b9269f93c47d64f8cc18e
ScrutchWasTaken <estelle.moncollin@gmail.com>
2016-02-10 14:42:53 +0100

    All sensors implemented + cleaning code

--- app/src/main/java/fr/scrutch/estelle/vmsalpha/MultipleSensorClickedActivity.java
+++ app/src/main/java/fr/scrutch/estelle/vmsalpha/MultipleSensorClickedActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,12 +18,7 @@ import java.util.List;
  */
 public class MultipleSensorClickedActivity extends AppCompatActivity {
 
-    private int count = 0;
     private SensorManager aSensorManager;   //one sensormanager for all listeners
-    //    private SensorManager bSensorManager;
-    private Sensor aSensor;
-    private long time1,time2,time3;
-    //    private int[] index;
     private SensorManager mSensorManager;   //don't touch that one
     private List<Sensor> sensorClicked;
     private ArrayList<Integer> index = new ArrayList<Integer>();
@@ -41,31 +35,22 @@ public class MultipleSensorClickedActivity extends AppCompatActivity {
         this.setContentView(R.layout.acceleration);
         //mSensorManager is just for the list of sensors, no other use
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);  //get the list of sensors
-//        bSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensorClicked = mSensorManager.getSensorList(Sensor.TYPE_ALL);
 
         Intent intent = getIntent();
-        //Get the int[] from the putExtra in the MainActivity.java
+        //Get the index as arraylist<Integer> from the putExtra in the MainActivity.java
         index = intent.getIntegerArrayListExtra("index");
         System.out.println("index :"+index);
         //We need the sensor manager to get the list of sensors in which the index is related to
         aSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
-//        textview.setText(sensorClicked.get(sensorType[0]).toString());    //Test: return the right sensor ;)
-        // magnetic then accelero if clicked accelero then magnetic
-//        for(int i = 0; i < index.size(); i++) {
-//            System.out.println(sensorClicked.get(index.get(i)).toString());
-//        }
-        System.out.println("##############"+sensorClicked.get(index.get(0)).toString()+"erfed"+sensorClicked.get(index.get(1)).toString());
         //Create the list with the selected sensors:
         for(int i = 0; i < index.size(); i++) {
             sensorsToListen.add(sensorClicked.get(index.get(i)));
-            System.out.println(sensorsToListen.get(i));
         }
 
-
         sensorThread = new SensorEventThread("SensorThread"); //TEST ###################
-        setListeners(sensorsToListen);
+//        setListeners(sensorsToListen);
     }
 
     @Override
@@ -93,117 +78,22 @@ public class MultipleSensorClickedActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        // register this class as a listener for the orientation and
-        // accelerometer sensors LISTENER
-        //TEST ################ decomment next line
-//        aSensorManager.registerListener(this,aSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.SENSOR_DELAY_FASTEST);
-        //en avoir plein => correspond au nbr de sensors selected
-
-//        aSensorManager.registerListener(sensorThread,aSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), 0,sensorThread.getHandler());
-
+        setListeners(sensorsToListen);
     }
 
     @Override
     protected void onPause() {
         // unregister listener
         super.onPause();
-        //TEST ############## next line
-//        aSensorManager.unregisterListener(this);
-
         sensorThread.quitLooper();
-//        bSensorManager.unregisterListener(this);
 
     }
 
-
-    ////////////////////////Other sensors:
-    private void getAccelerometer(SensorEvent event) {
-        count = (count+1)%300;  //to print just 1 value/300
-        //We have to get the textView of the layout
-//        TextView textView =  (TextView) findViewById(R.id.textView);
-//        TextView textView2 =  (TextView) findViewById(R.id.textView2);
-//        TextView textView3 =  (TextView) findViewById(R.id.textView3);
-        TextView textView4 = (TextView) findViewById(R.id.textView4);
-
-        if(count==0)
-            time1 = System.nanoTime();     //time before getting values
-//            time1 = sensorClicked.get(sensorType[0]).getMaxDelay(); //minDelay = 10000 ms
-
-        float[] values = event.values;
-        // Movement, get values
-        float x = values[0];
-        float y = values[1];
-        float z = values[2];
-
-        if(count==0)
-            time2 = System.nanoTime();
-
-
-//        time2 = event.timestamp;      //time after getting values
-//            //Toast.makeText(this, "x:"+x+"\n"+"y:"+y+"\n"+"z:"+z, Toast.LENGTH_SHORT).show();
-//            //R.id.textView(Long.toString(time1));
-//            //the force of gravity must be eliminated => problem of the direction of the phone
-
-//        textView.setText("t1 = "+Long.toString(time1)+" ns");
-////        setContentView(textViewTime2);
-//        if(count==0)
-//            time3 = System.nanoTime();
-//        time3 = event.timestamp;
-        //time to print, printed on screen:
-//        textView2.setText("t2 = "+Long.toString(time2)+" ns");
-//        textView3.setText("t3 = "+Long.toString(time3)+" ns");
-        textView4.setText("Accelerometer" + "\n" + "x:" + x + " m.s²" + "\n" + "y:" + y + " m.s²" + "\n" + "z:" + z + " m.s²");
-
-    }
-
-
-    private void getMagnetic(SensorEvent event) {
-        count = (count+1)%300;
-//        TextView textView =  (TextView) findViewById(R.id.textView);
-//        TextView textView2 =  (TextView) findViewById(R.id.textView2);
-        TextView textView3 =  (TextView) findViewById(R.id.textView3);
-//        TextView textView4 = (TextView) findViewById(R.id.textView4);
-
-        if(count==0)
-            time1 = System.nanoTime();     //time before getting values
-
-        float[] values = event.values;
-        //micro-Tesla µT ou uT
-        float x = values[0];
-        float y = values[1];
-        float z = values[2];
-        //missing y and z
-
-//        if(count==0)
-//            time2 = System.nanoTime();
-
-//        textView.setText(Long.toString(time1));
-//        if(count==0)
-//            time3 = System.nanoTime();
-//
-//        textView2.setText(Long.toString(time2));
-//        textView3.setText(Long.toString(time3));
-        textView3.setText("Ambiant magnetic field : " + x + " µT" +"\n"+ y + " µT" +"\n"+ z + " µT");
-
-
-    }
-
-    protected  void getSensorType(SensorEvent event){
-        TextView textView =  (TextView) findViewById(R.id.textView);
-        TextView textView2 =  (TextView) findViewById(R.id.textView2);
-        TextView textView3 =  (TextView) findViewById(R.id.textView3);
-//        TextView textView4 = (TextView) findViewById(R.id.textView4);
-        textView.setText(sensorClicked.get(index.get(0)).toString());
-        textView2.setText(sensorClicked.get(index.get(1)).toString());
-//        textView3.setText(sensorClicked.get(index.get(2)).toString());
-    }
 
     public void setListeners(ArrayList<Sensor> sensors){
         for (Sensor s:sensors) {
-            aSensorManager.registerListener(sensorThread,aSensorManager.getDefaultSensor(s.getType()), 0,sensorThread.getHandler());
-            System.out.println("COUCOU");
+            aSensorManager.registerListener(sensorThread, aSensorManager.getDefaultSensor(s.getType()), 0,sensorThread.getHandler());
         }
     }
 
-    /////////////////////// End test
 }
--- app/src/main/java/fr/scrutch/estelle/vmsalpha/SensorEventThread.java
+++ app/src/main/java/fr/scrutch/estelle/vmsalpha/SensorEventThread.java
@@ -20,15 +20,26 @@ public class SensorEventThread extends HandlerThread implements SensorEventListe
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-//        Log.v("SensorTest", "onSensorChanged");
         if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
-//            getAccelerometer(event);
+            getLinearAcceleration(event);
         } else if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            getGyroscope(event);
         } else if (event.sensor.getType() == Sensor.TYPE_GRAVITY) {
+            getGravity(event);
         } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
             getMagnetic(event);
         } else if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
             getAccelerometer(event);
+        } else if (event.sensor.getType() == Sensor.TYPE_LIGHT){
+            getLight(event);
+        } else if (event.sensor.getType() == Sensor.TYPE_PRESSURE){
+            getPressure(event);
+        } else if (event.sensor.getType() == Sensor.TYPE_PROXIMITY){
+            getProximity(event);
+        } else if (event.sensor.getType() == Sensor.TYPE_RELATIVE_HUMIDITY){
+            getRelativeHumidity(event);
+        } else if (event.sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE){
+            getTemperature(event);
         }
 
     }
@@ -64,6 +75,22 @@ public class SensorEventThread extends HandlerThread implements SensorEventListe
         System.out.println("Accelerometer" + "\n" + "x:" + x + " m.s²" + "\n" + "y:" + y + " m.s²" + "\n" + "z:" + z + " m.s²");
     }
 
+    private void getGravity(SensorEvent event){
+        float[] values = event.values;
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+        System.out.println("Gravity" + "\n" + "x:" + x + " m.s²" + "\n" + "y:" + y + " m.s²" + "\n" + "z:" + z + " m.s²");
+    }
+
+    private void getGyroscope(SensorEvent event){
+        float[] values = event.values;
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+        System.out.println("Gyroscope" + "\n" + "x:" + x + " rad/s" + "\n" + "y:" + y + " rad/s" + "\n" + "z:" + z + " rad/s");
+    }
+
     private void getMagnetic(SensorEvent event) {
         float[] values = event.values;
         //micro-Tesla µT ou uT
@@ -73,5 +100,42 @@ public class SensorEventThread extends HandlerThread implements SensorEventListe
         System.out.println("Ambiant magnetic field : " + x + " µT" +"\n"+ y + " µT" +"\n"+ z + " µT");
     }
 
+    private void getLinearAcceleration(SensorEvent event){
+        float[] values = event.values;
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+        System.out.println("Linear Acceleration" + "\n" + "x:" + x + " m.s²" + "\n" + "y:" + y + " m.s²" + "\n" + "z:" + z + " m.s²");
+    }
+
+    private void getLight(SensorEvent event){
+        float[] values = event.values;
+        float x = values[0];
+        System.out.println("Light" + "\n" + "x:" + x + " lx");
+    }
+
+    private void getPressure(SensorEvent event){
+        float[] values = event.values;
+        float x = values[0];
+        System.out.println("Pressure" + "\n" + "x:" + x + " hPa");
+    }
+
+    private void getProximity(SensorEvent event){
+        float[] values = event.values;
+        float x = values[0];
+        System.out.println("Proximity" + "\n" + "x:" + x + " cm");
+    }
+
+    private void getRelativeHumidity(SensorEvent event){
+        float[] values = event.values;
+        float x = values[0];
+        System.out.println("Relative Humidity" + "\n" + "x:" + x + " %");
+    }
+
+    private void getTemperature(SensorEvent event){
+        float[] values = event.values;
+        float x = values[0];
+        System.out.println("Temperature" + "\n" + "x:" + x + " °C = " + (x+273.15) + " K");
+    }
 
 }
